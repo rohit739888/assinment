e199681c3a152010b95925dbf6bbd0eb08d340f4
rohit739888 <rohit739888@gmail.com>
2023-09-05 19:38:43 +0530

    first commit

---
+++ assignment.js
@@ -0,0 +1,20 @@
+// simple interst
+let principal=Number(prompt("enter ammount"));
+let rate=Number(prompt("enter rate"));
+let time=Number(prompt("time period"));
+alert((principal*rate*time)/100);
+
+//VOLUME OF CUBE
+let edge=Number(prompt("enter edge"));
+alert((6*edge*edge));
+
+// surface Area of Cylinder
+
+let Radius=Number(prompt("enter Radius"));
+let height=Number(prompt("enter Height"));
+alert((2*Math.PI*Radius*height));
+
+// area of circle
+
+let radius=Number(prompt("enter radius"));
+alert((Math.PI*radius*radius));
